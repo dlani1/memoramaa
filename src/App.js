579a7f286b86a8f9cd906058adc9acf406ee3aac
DaniUwU <132004670+dlani1@users.noreply.github.com>
2024-03-13 15:43:43 -0600

    ADD Proyecto Memo

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import './App.css';
+import { useEffect, useState } from 'react';
+import Board from './Components/Board/Board';
+const emojiList = [...'🤣🦢🌹🍄🍓🍷👑💋'];
+
+const App = () => {
+
+  const [shuffledMemoBlocks, setShuffledMemoBlocks] = useState([]);
+  const [selectedMemoBlock, setselectedMemoBlock] = useState(null);
+  const [animating, setAnimating] = useState(false);
+
+
+useEffect( () => {
+  const shuffledEmojiList = shuffleArray([...emojiList, ...emojiList]);
+
+  setShuffledMemoBlocks(shuffledEmojiList.map( (emoji, i) => ({ index: i, emoji, flipped: false})));
+}, []);
+
+  const shuffleArray = a => {
+    for (let i = a.length - 1; i > 0; i--){
+      const j = Math.floor(Math.random() * (i + 1));
+      [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a;
+  }
+
+  const handleMemoClick = memoBlock =>{
+    //damos vuelta
+      const flippedMemoBlock = { ...memoBlock, flipped: true };
+      let shuffledMemoBlockCopy = [...shuffledMemoBlocks];
+      shuffledMemoBlockCopy.splice(memoBlock.index, 1 , flippedMemoBlock);
+      setShuffledMemoBlocks(shuffledMemoBlockCopy);
+      if(selectedMemoBlock === null) {
+        setselectedMemoBlock(memoBlock);
+      }else if(selectedMemoBlock.emoji === memoBlock.emoji) {
+        setselectedMemoBlock(null);
+      }else{
+        setAnimating(true);
+        setTimeout(() => {
+          shuffledMemoBlockCopy.splice(memoBlock.index, 1, memoBlock);
+          shuffledMemoBlockCopy.splice(selectedMemoBlock.index, 1, selectedMemoBlock);
+          setShuffledMemoBlocks(shuffledMemoBlockCopy);
+          setselectedMemoBlock(null);
+          setAnimating(false);
+        }, 1000);
+      }
+  }
+
+  return(
+    <Board memoBlocks={shuffledMemoBlocks} animating={animating} handleMemoClick={handleMemoClick}/>
+  );
+  }
+
+export default App;
